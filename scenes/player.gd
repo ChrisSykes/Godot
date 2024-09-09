42406751c107c243f540bf43f5a3327c2cbb355e
ChrisSykes <chris.sykes78@gmail.com>
2024-09-09 12:10:29 +0200

    First commit, player movement/ball bouncing

---
+++ scenes/player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+
+var speed = 300
+
+
+func get_input():
+	var direction := Input.get_axis("up", "down" )
+	
+	if direction:
+		velocity.y = direction * speed
+		print("Key pressed")
+		print(velocity.y)
+	else: 
+		velocity.y = 0
+		print(velocity.y)
+
+func _physics_process(_delta):
+	get_input()
+	move_and_slide()
